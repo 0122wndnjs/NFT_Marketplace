dd613fc663442f6e9be5914d3f1467cd444b2b17
Joowon Kim <0122wndnjs@gmail.com>
2022-09-06 16:26:13 +0900

    Functionality to create a market item

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -34,4 +34,31 @@ contract NFTMarketplace is ERC721URIStorage {
         uint256 price,
         bool sold
     );
+
+    // Returns the listing price of the contract
+    function getListingPrice() public view returns (uint256) {
+        return listingPrice;
+    }
+
+    // Update the listing price
+    function updateListingPrice(uint _listingPrice) public payable {
+        require(owner == msg.sender, "Only marketplace owner can update listing price");
+        listingPrice = _listingPrice;
+    }
+
+    function createMarketItem(uint256 tokenId, uint256 price) private {
+        require(price > 0, "Price must be greater than zero");
+        require(msg.value == listingPrice, "Price must be equal to listing price");
+
+        idToMarketItem[tokenId] = MarketItem(
+            tokenId,
+            payable(msg.sender),
+            payable(address(this)),
+            price,
+            false
+        );
+
+        _transfer(msg.sender, address(this), tokenId);
+        emit MarketItemCreated(tokenId, msg.sender, address(this), price, false);
+    } 
 }

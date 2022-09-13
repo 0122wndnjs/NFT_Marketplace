c586b4a82623f8b62ed252b81bd7df3d1536fa5c
Joowon Kim <0122wndnjs@gmail.com>
2022-09-13 14:12:10 +0900

    Unit test to deploy the contract

---
+++ test/NFTMarket.js
@@ -0,0 +1,32 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("NFT Marketplace", function () {
+  let NFTMarket;
+  let nftMarket;
+  let listingPrice;
+  let contractOwner;
+  let buyerAddress;
+  let nftMarketAddress;
+
+  const auctionPrice = ethers.utils.parseUnits("100", "ether");
+
+  beforeEach(async () => {
+    NFTMarket = await ethers.getContractFactory("NFTMarketplace");
+    nftMarket = await NFTMarket.deploy();
+    await nftMarket.deployed();
+    nftMarketAddress = nftMarket.address;
+    [contractOwner, buyerAddress] = await ethers.getSigners();
+    listingPrice = await nftMarket.getListingPrice();
+    listingPrice = listingPrice.toString();
+  });
+
+  const mintAndListNFT = async (tokenURI, auctionPrice) => {
+    const transaction = await nftMarket.createToken(tokenURI, auctionPrice, {
+      value: listingPrice,
+    });
+    const receipt = await transaction.wait();
+    const tokenID = receipt.events[0].args.tokenId;
+    return tokenID;
+  };
+});

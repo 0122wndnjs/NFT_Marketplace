16a551ef0c6f54b8b047c1b24d3f60594fceb185
Joowon Kim <0122wndnjs@gmail.com>
2022-09-14 16:22:37 +0900

    minor css change

--- client/pages/index.js
+++ client/pages/index.js
@@ -87,7 +87,7 @@ export default function Home() {
       <div className="px-4" style={{ maxWidth: "1600px" }}>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 pt-4">
           {nfts.map((nft, i) => (
-            <div key={i} className="border shadow rounded-xl overflow-hidden">
+            <div key={i} className="border shadow rounded-xl overflow-hidden mx-5 my-5">
               <Image
                 src={nft.image}
                 alt={nft.name}

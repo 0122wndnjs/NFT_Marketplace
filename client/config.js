cb56415169418b08c04c414e80ca26529c70bcda
Joowon Kim <0122wndnjs@gmail.com>
2022-09-14 11:53:24 +0900

    Setup Pinata IPFS keys

--- client/config.js
+++ client/config.js
@@ -1,2 +1,4 @@
 export const contractAddress = "0xD295A42A527C2c4294de95E1fE41442B7ca7732C";
 export const INFURA_URL = "https://goerli.infura.io/v3/391237b8b03f42f6b19a2bf4a77b2e1c";
+export const PINATA_KEY = "c077b0c01f96da368c20";
+export const PINATA_SECRET = "5359bf971736ac48f219ccb5a6ef5cc8032f378bd851ba77c229ee3db614aba1";
